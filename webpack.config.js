1dc01012317439c430bbe7926e02e3c0566ad1a8
Daniil <daniil.petro3@gmail.com>
2020-02-07 16:49:34 +0500

    end styling header for mobile(without menu)

--- webpack.config.js
+++ webpack.config.js
@@ -139,7 +139,7 @@ module.exports = {
       },
        {
         // Fonts
-        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        test: /\.(woff(2)?|ttf|eot)(\?v=\d+\.\d+\.\d+)?$/,
         loader: "file-loader",
         options: {
           name: "[name].[ext]",
