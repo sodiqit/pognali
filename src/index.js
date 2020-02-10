9113a2a081b4310733426b78e0300f5eaa8a538e
Daniil <daniil.petro3@gmail.com>
2020-02-10 21:49:08 +0500

    write script for open/close mobile-menu

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 import './less/styles.less';
+
+const button = document.querySelector('.open-button');
+const menu = document.querySelector('.menu');
+
+menu.style.opacity = 0;
+menu.classList.remove('menu--no-js');
+
+button.addEventListener('click', () => {
+  button.classList.toggle('open-button--opened');
+  menu.classList.toggle('menu--opened');
+  menu.style.opacity = 1;
+});
