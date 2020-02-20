d1cc341170d6080bba4c6fa24190d53713c8d1ac
Daniil <daniil.petro3@gmail.com>
2020-02-20 22:53:12 +0500

    create custom animation without css

--- src/js/animation.js
+++ src/js/animation.js
@@ -1,21 +1,28 @@
 export default class Animate {
-  constructor(el, animation) {
+  constructor(el, animation, options = {}) {
     this._el= el;
     this._animation = animation;
+    this._options = options;
     this._animatesEnd = [];
 
-    // if (this._el instanceof NodeList) {
-    //   this._el.forEach(item => {
-    //     this._checkCoord(item);
-    //   });
-    // } else {
-    //   this._checkCoord(this._el);
-    // }
+    if (this._options) {
+      this._animateSettings = {
+        timing: {
+          linear(timeFraction) {
+            return timeFraction;
+          },
+          ease(timeFraction) {
+            return Math.pow(timeFraction, 2);
+          }
+        }
+      }
+    }
+    console.log(this._el instanceof NodeList);
     this._checkHandlerBind = this._checkHandler.bind(this);
     this._checkCoord();
   }
 
-  _bindAnimation(target, i = 1) {
+  _bindAnimation(target, i = 0) {
     if (target) {
       target.classList.add(this._animation);
 
@@ -28,11 +35,9 @@ export default class Animate {
           end: true
         };
 
-        this._animatesEnd.push(animate);
+        this._animatesEnd[animate.index] = animate;
       }
 
-    } else if (target.classList.contain(this._animation)) {
-      return;
     } else {
       throw new Error('el not defined');
     }
@@ -66,21 +71,31 @@ export default class Animate {
   }
 
   _unBindHandler() {
-    if (this._el instanceof NodeList && this._el.length === this._animatesEnd.length) {
+    if (this._el instanceof NodeList && this._el.length === this._animatesEnd.length && this._animatesEnd[0] !== undefined) {
       window.removeEventListener('scroll', this._checkHandlerBind);
       console.log('removed');
-    } else {
-      console.log(1);
+    } else if (!(this._el instanceof NodeList) && this._animatesEnd.length > 0) {
+      window.removeEventListener('scroll', this._checkHandlerBind);
+      console.log('removed');
     }
   }
 
   _checkHandler() {
     if (this._el instanceof NodeList) {
       this._el.forEach((item, i) => {
-        this._watcher(item) ? this._bindAnimation(item, i) : null;
+        if (Object.keys(this._options).length !== 0) {
+          this._watcher(item) ? this._animate(item, i) : null;
+        } else {
+          this._watcher(item) ? this._bindAnimation(item, i) : null;
+        }
       });
     } else {
-      this._watcher(this._el) ? this._bindAnimation(this._el) : null;
+      if (Object.keys(this._options).length !== 0) {
+        this._watcher(this._el) ? this._animate() : null;
+      } else {
+        this._watcher(this._el) ? this._bindAnimation(this._el) : null;
+      }
+
     }
 
     this._unBindHandler();
@@ -90,4 +105,34 @@ export default class Animate {
     window.addEventListener('scroll', this._checkHandlerBind);
   }
 
+  _animate(item = this._el, i = 0) {
+
+    if (this._animatesEnd[i]) {
+      return;
+    }
+
+    let start = performance.now();
+
+    let animate = (time) => {
+      let timeFraction = (time - start) / this._options.duration;
+      if (timeFraction > 1) timeFraction = 1;
+
+      // вычисление текущего состояния анимации
+      let progress = this._animateSettings.timing[this._options.timing](timeFraction);
+
+      this._options.draw(item, progress, i) // отрисовать её
+
+      if (timeFraction < 1) {
+        requestAnimationFrame(animate);
+        this._animatesEnd[i] = {
+          index: i,
+          end: true
+        };
+
+        console.log(this._animatesEnd);
+      }
+    }
+
+    requestAnimationFrame(animate);
+  }
 }
