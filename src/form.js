5817312cc8eb3fc5dd0bf424b167f4262f4713e4
Daniil <daniil.petro3@gmail.com>
2020-03-16 16:55:55 +0500

    add new mixins and vars and include in main file

--- src/form.js
+++ src/form.js
@@ -2,3 +2,4 @@ import './less/styles-form.less';
 import './blocks/libs/header/header';
 
 import './blocks/form.blocks/common.blocks/profile/profile';
+import './blocks/form.blocks/common.blocks/add-plan/add-plan';
