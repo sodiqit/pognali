e1e2dbef11c3b9b3f25e476b4a738bee7c12cc33
Daniil <daniil.petro3@gmail.com>
2020-03-28 21:05:43 +0500

    create slider logic width input and mobile

--- src/blocks/catalog.blocks/common.blocks/travellers/__filter/__filter.js
+++ src/blocks/catalog.blocks/common.blocks/travellers/__filter/__filter.js
@@ -14,10 +14,53 @@ class RangeSlider {
     this._maxInput = inputs.querySelector('input[name="max"]');
     this._decreaseButton = this._slider.querySelector('.decrease-level');
     this._increaseButton = this._slider.querySelector('.increase-level');
+    this._activeBar = this._slider.querySelector('.travellers-filter__active');
 
     this.startDragDecr = this._startDragDecr.bind(this);
     this.startDragIncr = this._startDragIncr.bind(this);
+    this.changeDecrCoord = this._changeDecrCoord.bind(this);
+    this.changeIncrCoord = this._changeIncrCoord.bind(this);
     this._bind();
+  }
+
+  _changeDecrCoord(e) {
+    let newValue = parseInt(e.target.value);
+    let maxValue = parseInt(this._maxInput.value);
+    if (newValue >= maxValue) {
+      newValue = maxValue - 1;
+      e.target.value = maxValue - 1;
+    } else if (newValue <= 0) {
+      newValue = 0;
+      e.target.value = 0;
+    }
+    let sliderCoords = this._getCoords(this._slider);
+
+    let newLeft = sliderCoords.width / 100 * newValue;
+
+    this._activeBar.style.width = `${(this._increaseButton.getBoundingClientRect().left - sliderCoords.left) - newLeft}px`;
+    this._activeBar.style.left = `${newLeft}px`;
+    this._decreaseButton.style.left = `${newLeft}px`;
+  }
+
+  _changeIncrCoord(e) {
+    let newValue = parseInt(e.target.value);
+    let minValue = parseInt(this._minInput.value)
+    if (newValue <= minValue) {
+      newValue = minValue + 1;
+      e.target.value = minValue + 1;
+    } else if (newValue >= 100) {
+      newValue = 100;
+      e.target.value = 100;
+    }
+    let sliderCoords = this._getCoords(this._slider);
+
+    let newLeft = (sliderCoords.width / 100) * newValue;
+
+    console.log((this._decreaseButton.getBoundingClientRect().left - sliderCoords.left));
+
+    this._activeBar.style.width = `${newLeft - (this._decreaseButton.getBoundingClientRect().left - sliderCoords.left)}px`;
+    this._activeBar.style.left = `${this._decreaseButton.getBoundingClientRect().left - sliderCoords.left}px`;
+    this._increaseButton.style.left = `${newLeft}px`;
   }
 
   _getCoords(elem) {
@@ -31,7 +74,7 @@ class RangeSlider {
   }
 
   _startDragIncr(e) {
-    let coordParent = e.target.parentNode.getBoundingClientRect();
+    let coordParent = this._slider.getBoundingClientRect();
 
     e.target.ondragstart = () => {
       return false;
@@ -40,7 +83,14 @@ class RangeSlider {
     let activeBar = e.target.previousElementSibling;
 
     const changePos = (event) => {
-      let newLeft = event.clientX - coordParent.left;
+
+      let newLeft;
+
+      if (!event.clientX) {
+        newLeft = event.touches[0].clientX - coordParent.left;
+      } else {
+        newLeft = event.clientX - coordParent.left;
+      }
 
       if (newLeft <= this._decreaseButton.getBoundingClientRect().left - coordParent.left + 10) {
         newLeft = this._decreaseButton.getBoundingClientRect().left - coordParent.left + 10;
@@ -51,19 +101,24 @@ class RangeSlider {
       e.target.style.left=`${newLeft}px`;
       activeBar.style.width = `${(this._increaseButton.getBoundingClientRect().left - coordParent.left) - (this._decreaseButton.getBoundingClientRect().left - coordParent.left)}px`;
       activeBar.style.left = `${this._decreaseButton.getBoundingClientRect().left - coordParent.left}px`;
+      this._maxInput.value = Math.floor(newLeft / coordParent.width * 100);
     }
 
     const deleteList = () => {
       document.removeEventListener('mousemove', changePos);
+      document.removeEventListener('touchmove', changePos);
       document.removeEventListener('mouseup', deleteList);
+      document.removeEventListener('touchend', deleteList);
     }
 
     document.addEventListener('mousemove', changePos);
+    document.addEventListener('touchmove', changePos);
     document.addEventListener('mouseup', deleteList);
+    document.addEventListener('touchend', deleteList);
   }
 
   _startDragDecr(e) {
-    let coordParent = e.target.parentNode.getBoundingClientRect();
+    let coordParent = this._slider.getBoundingClientRect();
 
     let activeBar = e.target.nextElementSibling;
 
@@ -72,7 +127,13 @@ class RangeSlider {
     };
 
     const changePos = (event) => {
-      let newLeft = event.clientX - coordParent.left;
+      let newLeft;
+
+      if (!event.clientX) {
+        newLeft = event.touches[0].clientX - coordParent.left;
+      } else {
+        newLeft = event.clientX - coordParent.left;
+      }
 
       if (newLeft < 0) {
         newLeft = 0;
@@ -85,20 +146,30 @@ class RangeSlider {
       e.target.style.left=`${newLeft}px`;
       activeBar.style.width = `${(this._increaseButton.getBoundingClientRect().left - coordParent.left) - newLeft}px`;
       activeBar.style.left = `${newLeft}px`;
+      console.log(coordParent.width, newLeft);
+      this._minInput.value = Math.ceil(newLeft / coordParent.width * 100);
     }
 
     const deleteList = () => {
       document.removeEventListener('mousemove', changePos);
+      document.removeEventListener('touchmove', changePos);
       document.removeEventListener('mouseup', deleteList);
+      document.removeEventListener('touchend', deleteList);
     }
 
     document.addEventListener('mousemove', changePos);
+    document.addEventListener('touchmove', changePos);
     document.addEventListener('mouseup', deleteList);
+    document.addEventListener('touchend', deleteList);
   }
 
   _bind() {
     this._decreaseButton.addEventListener('mousedown', this.startDragDecr);
+    this._decreaseButton.addEventListener('touchstart', this.startDragDecr);
     this._increaseButton.addEventListener('mousedown', this.startDragIncr);
+    this._increaseButton.addEventListener('touchstart', this.startDragIncr);
+    this._minInput.addEventListener('change', this.changeDecrCoord);
+    this._maxInput.addEventListener('change', this.changeIncrCoord);
   }
 }
 
